7f1585dfa300510dda2e5db81e865ad0c423b6e1
sespina <sespina@itba.edu.ar>
2021-08-29 20:54:12 -0300

    ahora se corre con jetty porque le agregamos el plugin

--- myApp/webapp/src/main/java/ar/edu/itba/paw/webapp/Controller/HelloWorldController.java
+++ myApp/webapp/src/main/java/ar/edu/itba/paw/webapp/Controller/HelloWorldController.java
@@ -13,7 +13,7 @@ public class HelloWorldController {
     @RequestMapping("/")
     public ModelAndView helloWorld() {
         final ModelAndView mav = new ModelAndView("index");
-        mav.addObject("greeting", userService.list().get(0).getName());
+        mav.addObject("greeting", userService.list().get(1).getName());
         return mav;
     }
 }
