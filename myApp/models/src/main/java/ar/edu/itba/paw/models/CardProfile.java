0204920c6884e6415dfe3172558341ede1bd3cb5
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2021-09-07 23:10:31 -0300

    getters y setters

--- myApp/models/src/main/java/ar/edu/itba/paw/models/CardProfile.java
+++ myApp/models/src/main/java/ar/edu/itba/paw/models/CardProfile.java
@@ -4,6 +4,38 @@ public class CardProfile {
     private int userId, price;
     private String name, subject;
 
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
     public CardProfile (int userId, String name, String subject, int price) {
         this.userId = userId;
         this.name = name;
