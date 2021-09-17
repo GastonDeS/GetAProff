8529bad21c07b61d887459a542a9c8ea1efe575a
Agustin Naso <anaso@itba.edu.ar>
2021-09-17 16:51:26 -0300

    /tutors acepta filtros. Botones para manejo de estos

--- myApp/webapp/src/main/webapp/resources/js/script.js
+++ myApp/webapp/src/main/webapp/resources/js/script.js
@@ -1,8 +1,59 @@
-const keepButtonFocused = () => {
+
+const resetFilters = () => {
+    let url = window.location.href;
+    console.log(url);
+    console.log(location.search);
+    url = url.substr(0, url.indexOf('&'));
+    window.location.href = url;
+}
+
+
+const keepPriceButtonFocused = () => {
     document.getElementById('priceDropdownButton').focus();
 }
 
-const updatePrice  = (price) => {
-    document.getElementById('priceDisplay').innerHTML = 'Precio: $' + price;
+const showFilterButton = () => {
+    document.getElementById('filter-button').style.display = 'block';
+
 }
+
+const updateLevel = (value) => {
+    if(value === "1")
+        value = 'Primario';
+    else if (value === "2")
+        value = 'Secundario';
+    else
+        value = 'Universitario';
+    document.getElementById('levelDropdownButton').firstChild.data = 'Nivel: ' + value;
+
+}
+
+const elem = document.getElementsByTagName('input');
+    for( i = 0 ; i < elem.length ; i++)
+        elem[i].addEventListener('change', showFilterButton);
+
+
+const updatePrice  = (price, defaultValue) => {
+    if(price === null)
+        price = defaultValue;
+    console.log(price);
+    document.getElementById('priceDisplay').innerHTML = 'Precio: $' + price;
+    document.getElementById('priceDropdownButton').firstChild.data = 'Precio: $' + price;
+}
+
+const checkFilters = () => {
+    const urlParams = new URLSearchParams(location.search);
+    if(urlParams.get('price') !== null) {
+        updatePrice(urlParams.get('price'),1);
+    }
+    if(urlParams.get('level') !== null) {
+        updateLevel(urlParams.get('level'));
+    }
+    if(urlParams.get('price') !== null || urlParams.get('level') !== null) {
+        document.getElementById('clear-filter-button').style.display= "block";
+    }
+}
+window.onload = checkFilters;
+
+
 
