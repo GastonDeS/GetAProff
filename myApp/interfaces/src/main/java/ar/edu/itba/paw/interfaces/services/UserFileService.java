3f34189b3627152a6f6ecf0b06a26f2d10c0b855
sespinaItba <sespina@itba.edu.ar>
2021-11-03 13:20:12 -0300

    sacando funciones que no se usan

--- myApp/interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/UserFileService.java
+++ myApp/interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/UserFileService.java
@@ -2,7 +2,7 @@ package ar.edu.itba.paw.interfaces.services;
 
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.models.UserFile;
-import org.springframework.web.multipart.MultipartFile;
+
 
 import java.io.IOException;
 import java.util.List;
@@ -11,9 +11,6 @@ public interface UserFileService {
     List<UserFile> getAllUserFiles(Long ownerId);
 
     UserFile saveNewFile(byte[] file, String fileName, Long ownerId);
-
-    List<UserFile> saveMultipleNewFiles(MultipartFile[] files, Long ownerId) throws IOException;
-
     int deleteFile(Long fileId);
 
 }
