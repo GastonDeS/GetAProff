25a23f6a8975cb0c34f201b12e05ae2ec079e3de
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-04-21 16:32:05 -0300

    SubjectLevelDto mejorado

--- myApp/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/SubjectLevelDto.java
+++ myApp/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/SubjectLevelDto.java
@@ -7,25 +7,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class SubjectLevelDto {
-
-    private SubjectDto subject;
+public class SubjectLevelDto extends SubjectDto{
 
     private List<Integer> levels;
+    private String name, url;
+    private Long subjectId;
 
     public static SubjectLevelDto fromSubjectLevel(UriInfo uri, Map.Entry<Subject, List<Integer>> subjectAndLevels) {
         SubjectLevelDto subjectLevelDto = new SubjectLevelDto();
-        subjectLevelDto.subject = SubjectDto.get(uri, subjectAndLevels.getKey());
         subjectLevelDto.levels = subjectAndLevels.getValue().stream().sorted().collect(Collectors.toList());
+        subjectLevelDto.name = subjectAndLevels.getKey().getName();
+        subjectLevelDto.subjectId = subjectAndLevels.getKey().getSubjectId();
+        subjectLevelDto.url = uri.getBaseUriBuilder().path("subjects")
+                .path(String.valueOf(subjectAndLevels.getKey().getSubjectId())).build().toString();
         return subjectLevelDto;
-    }
-
-    public SubjectDto getSubject() {
-        return subject;
-    }
-
-    public void setSubject(SubjectDto subject) {
-        this.subject = subject;
     }
 
     public List<Integer> getLevels() {
@@ -34,5 +29,35 @@ public class SubjectLevelDto {
 
     public void setLevels(List<Integer> levels) {
         this.levels = levels;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public Long getSubjectId() {
+        return subjectId;
+    }
+
+    @Override
+    public void setSubjectId(Long subjectId) {
+        this.subjectId = subjectId;
     }
 }
