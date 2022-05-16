3a29f822d9011d11c08c9b95db32f945a3bbde6b
linbrittany <brlin@itba.edu.ar>
2022-05-16 16:22:14 -0300

    fix: pequeños arreglos front

--- myApp/getaproff/src/views/RequestSubject/index.js
+++ myApp/getaproff/src/views/RequestSubject/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 
 import { Wrapper, MainContainer, Title } from '../../GlobalStyle';
 import { Content, InputContainer } from './RequestSubject.styles';
@@ -8,6 +7,16 @@ import Button from '../../components/Button';
 import i18next from "i18next";
 
 const RequestSubject = () => {
+  const [subject, setSubject] = useState();
+  const [subjectError, setSubjectError] = useState(false);
+  const [text, setText] = useState();
+  const [textError, setTextError] = useState(false);
+
+  const submitRequest = () => {
+    if (!subject) setSubjectError(true);
+    if (!text) setTextError(true);
+  }
+
   return (
     <Wrapper>
       <Navbar empty={true} />
@@ -15,12 +24,18 @@ const RequestSubject = () => {
         <Content>
           <Title>{i18next.t('requestSubject.title')}</Title>
           <InputContainer>
-            <p>{i18next.t('requestSubject.insertSubject')}</p>
-            <input placeholder={i18next.t('requestSubject.insertSubjectPlaceholder')}/>
-            <p>{i18next.t('requestSubject.insertMessage')}</p>
-            <textarea placeholder={i18next.t('requestSubject.insertMessagePlaceholder')}/>
+            <h3>{i18next.t('requestSubject.insertSubject')}</h3>
+            <input placeholder={i18next.t('requestSubject.insertSubjectPlaceholder')} onChange={(e) => setSubject(e.target.value)}/>
+            {
+              subjectError && <p>{i18next.t('form.requiredField')}</p>
+            }
+            <h3>{i18next.t('requestSubject.insertMessage')}</h3>
+            <textarea placeholder={i18next.t('requestSubject.insertMessagePlaceholder')} onChange={(e) => setText(e.target.value)}/>
+            {
+              textError && <p>{i18next.t('form.requiredField')}</p>
+            }
           </InputContainer>
-          <Button text={i18next.t('requestSubject.sendRequest')} fontSize='1rem'/>
+          <Button text={i18next.t('requestSubject.sendRequest')} fontSize='1rem' callback={submitRequest}/>
         </Content>
       </MainContainer>
     </Wrapper>
