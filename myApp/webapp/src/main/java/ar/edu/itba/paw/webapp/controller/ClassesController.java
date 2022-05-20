fd632af1fca19ca8f52a80adc33e80309b32809d
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-05-20 01:59:30 -0300

    feat: All Exceptions Catcher

--- myApp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ClassesController.java
+++ myApp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ClassesController.java
@@ -8,7 +8,6 @@ import ar.edu.itba.paw.models.Lecture;
 import ar.edu.itba.paw.models.Page;
 import ar.edu.itba.paw.webapp.dto.ClassroomDto;
 import ar.edu.itba.paw.webapp.exceptions.ConflictException;
-import ar.edu.itba.paw.webapp.exceptions.InternalServerErrorException;
 import ar.edu.itba.paw.webapp.requestDto.ClassRequestDto;
 import ar.edu.itba.paw.webapp.security.services.AuthFacade;
 import ar.edu.itba.paw.webapp.util.ConflictStatusMessages;
@@ -56,33 +55,26 @@ public class ClassesController {
                                        @QueryParam("status") @DefaultValue("-1") int status,
                                        @QueryParam("page") @DefaultValue("1") Integer page,
                                        @QueryParam("pageSize") @DefaultValue("10") Integer pageSize) {
-        try {
-            Long userId = authFacade.getCurrentUserId();
-            final Page<Lecture> lectures = lectureService.findClasses(userId, asTeacher, status, page, pageSize);
-            Response.ResponseBuilder builder = Response.ok(
-                    new GenericEntity<List<ClassroomDto>>(lectures.getContent().stream()
-                            .map(ClassroomDto::getClassroom)
-                            .collect(Collectors.toList())) {
-                    });
-            return PaginationBuilder.build(lectures, builder, uriInfo, pageSize);
-        } catch (Exception e) {
-            throw new InternalServerErrorException("");
-        }
+        Long userId = authFacade.getCurrentUserId();
+        final Page<Lecture> lectures = lectureService.findClasses(userId, asTeacher, status, page, pageSize);
+        Response.ResponseBuilder builder = Response.ok(
+                new GenericEntity<List<ClassroomDto>>(lectures.getContent().stream()
+                        .map(ClassroomDto::getClassroom)
+                        .collect(Collectors.toList())) {
+                });
+        return PaginationBuilder.build(lectures, builder, uriInfo, pageSize);
+
     }
 
     @POST
     @Consumes("application/vnd.getaproff.api.v1+json")
     public Response requestClass(ClassRequestDto classRequestDto) {
-        try {
-            Lecture newLecture = lectureService.create(authFacade.getCurrentUserId(), classRequestDto.getTeacherId(), classRequestDto.getLevel(),
-                    classRequestDto.getSubjectId(), classRequestDto.getPrice()).orElseThrow(() -> new ConflictException(ConflictStatusMessages.LECTURE_CREATE));
-            LOGGER.debug("requested class of subject with id {} from teacher with id{}, level: {}, price: {}", classRequestDto.getSubjectId(),classRequestDto.getTeacherId(), classRequestDto.getLevel(), classRequestDto.getPrice());
-            emailService.sendNewClassMessage(newLecture.getTeacher().getMail(), newLecture.getStudent().getName(), newLecture.getSubject().getName(), newLecture.getClassId(), uriInfo.getBaseUri().toString());
-            URI location = URI.create(uriInfo.getBaseUri() + "classroom/" + newLecture.getClassId());
-            return Response.created(location).build();
-        } catch (Exception e) {
-            throw new InternalServerErrorException("");
-        }
+        Lecture newLecture = lectureService.create(authFacade.getCurrentUserId(), classRequestDto.getTeacherId(), classRequestDto.getLevel(),
+                classRequestDto.getSubjectId(), classRequestDto.getPrice()).orElseThrow(() -> new ConflictException(ConflictStatusMessages.LECTURE_CREATE));
+        LOGGER.debug("requested class of subject with id {} from teacher with id{}, level: {}, price: {}", classRequestDto.getSubjectId(),classRequestDto.getTeacherId(), classRequestDto.getLevel(), classRequestDto.getPrice());
+        emailService.sendNewClassMessage(newLecture.getTeacher().getMail(), newLecture.getStudent().getName(), newLecture.getSubject().getName(), newLecture.getClassId(), uriInfo.getBaseUri().toString());
+        URI location = URI.create(uriInfo.getBaseUri() + "classroom/" + newLecture.getClassId());
+        return Response.created(location).build();
     }
 
 }
